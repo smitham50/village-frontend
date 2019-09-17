701060501381bd29524fddcd407f62c5fe311a4d
Aaron Smith <smitham50@gmail.com>
2019-09-17 15:39:23 -0400

    added communityboard and profile components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import CommunityBoard from './containers/communityboard'
+import Profile from './containers/profile'
 
 function App() {
   return (
     <div className="App">
-      
+      <Profile />
+      <CommunityBoard />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/containers/communityboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class CommunityBoard extends React.Component {
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
