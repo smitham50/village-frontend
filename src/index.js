57039ba927bb5ba70a4115ac4d78fd829e90b112
Aaron Smith <smitham50@gmail.com>
2019-09-17 15:53:41 -0400

    added userreducer

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import userReducer from './reducers/userreducer'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootReducer = combineReducers({ user: userReducer })
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const store = createStore(rootReducer)
+
+ReactDOM.render(<Provider store={store}><Router><Route path="/" component={App} /></Router></Provider>, document.getElementById('root'));
+
 serviceWorker.unregister();
---
+++ src/reducers/userreducer.js
@@ -0,0 +1,13 @@
+const defaultState = {
+  
+}
+
+function userReducer(prevState = defaultState, action) {
+  switch (action.type) {
+    default:
+      return prevState
+  }
+
+}
+
+export default userReducer
