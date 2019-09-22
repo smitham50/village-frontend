4cd275f80b049a5f0e22908c425b79c17c8541b4
Aaron Smith <smitham50@gmail.com>
2019-09-22 10:51:48 -0400

    added requests and request files

---
+++ src/containers/requests.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Request from '../components/request'
+
+class Requests extends React.Component {
+  render() {
+    return (
+      <Request />
+    )
+  }
+}
+
+export default Requests
